d50efa4cd9a29cc0aae25ed20cc667fb956ff5f4
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2022-04-29 10:09:26 +0200

    Poprawka dla nazwy obiektów

--- src/generate_js/generate_params_type.rs
+++ src/generate_js/generate_params_type.rs
@@ -9,6 +9,23 @@ fn add_require(require: bool, type_param: impl Into<String>) -> String {
     }
 }
 
+fn generate_object_prop_name(key: &String) -> String {
+    let first = key.chars().next();
+
+    match first {
+        Some(first) => {
+            if first.is_alphabetic() {
+                key.to_string()
+            } else {
+                format!("'{key}'")
+            }
+        },
+        None => {
+            panic!("the property name of the object must be a non-empty string");
+        }
+    }
+}
+
 pub fn generate_type_ts(ident: u32, type_param: &OpenApiType) -> String {
     let right = '}';
 
@@ -32,6 +49,7 @@ pub fn generate_type_ts(ident: u32, type_param: &OpenApiType) -> String {
             for (key, value) in props.get_sorted() {
                 let ident_str = generate_ident(next_ident);
                 let value_std = generate_type_ts(next_ident, value);
+                let key = generate_object_prop_name(key);
                 out.push(format!("{ident_str}{key}: {value_std},"));
             }
 
